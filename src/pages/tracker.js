c93414b09f92a427818b91dd62e59a6a35da10c6
Edison611 <edisony611@gmail.com>
2023-07-27 22:26:05 -0400

    feat : add robot placer

--- src/pages/tracker.js
+++ src/pages/tracker.js
@@ -5,6 +5,65 @@ import field from '../images/field.png'
 import skills_field from '../images/skills-field.png'
 import ToggleSwitch from '../components/ToggleSwitch';
 
+
+const BotDrawer = ({ x, y, width, height, rotation, showText }) => {
+    const squareStyle = {
+      position: 'absolute',
+      left: `${x}px`,
+      top: `${y}px`,
+      width: `${width}px`,
+      height: `${height}px`,
+      backgroundColor: 'red',
+      opacity: 0.8,
+      transform: `rotate(${rotation}deg)`, // Apply rotation to the square
+      pointerEvents: 'none', // Make the square non-interactable
+    };
+  
+    const topLineStyle = {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '1px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  
+    const bottomLineStyle = {
+      position: 'absolute',
+      bottom: '0',
+      left: '0',
+      width: '100%',
+      height: '1px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  
+    const textStyle = {
+      fontSize: '1.2vw',
+      fontWeight: 'bold',
+      color: 'yellow',
+      textTransform: 'uppercase',
+      padding: '2px 6px',
+      opacity: 1,
+      display: showText ? 'block' : 'none',
+    };
+  
+    return (
+        <div style={squareStyle}>
+        <div style={topLineStyle}>
+          <div style={textStyle}>FRONT</div>
+        </div>
+        <div style={bottomLineStyle}>
+          <div style={textStyle}>BACK</div>
+        </div>
+      </div>
+    );
+  };
+
+
 const Tracker = () => {
     const [localMousePos, setLocalMousePos] = useState({});
     const [skills, setSkills] = useState(false);
@@ -17,10 +76,22 @@ const Tracker = () => {
         setLocalMousePos({ x: localX, y: localY });
     };
 
-    function convert_coord(x, y) {
+    function mouseToCoord(x, y) {
         x = (((x - (windowSize[0]/5))/3.542) * 1280/windowSize[0]).toFixed(0);
         y = ((-(y - (windowSize[1]/2.25))/3.2361) * 585/windowSize[1]).toFixed(0);
         return [x, ", ",y]
+    }
+
+    function coordToMouse(x, y) {
+        x = ((x * 3.542 * windowSize[0])/(1280)+windowSize[0]/5) + windowSize[0]/3.3333;
+        y = -((y * 3.2361 * windowSize[1])/585 - windowSize[1]/2.25);
+        return [x, y]
+    }
+
+    function valToSize(w, h) {
+        w = w * 3.542 * windowSize[0]/1280;
+        h = h * 3.542 * windowSize[1]/585;
+        return [w, h]
     }
 
     const handleToggle = (switchState) => {
@@ -48,23 +119,120 @@ const Tracker = () => {
         };
     }, []);
 
+    // Add the width and height states for the square
+    const [squareWidth, setSquareWidth] = useState(10);
+    const [squareHeight, setSquareHeight] = useState(12);
+
+    const [startPos, setStartPos] = useState([0, 0]);
+
+    const handleSquareWidthChange = (event) => {
+        setSquareWidth(parseInt(event.target.value));
+    };
+
+    const handleSquareHeightChange = (event) => {
+        setSquareHeight(parseInt(event.target.value));
+    };
+
+    const handleStartingPosX = (event) => {
+        setStartPos([parseInt(event.target.value), startPos[1]])
+    }
+
+    const handleStartingPosY = (event) => {
+        
+        setStartPos([startPos[0], parseInt(event.target.value)])
+    }
+
+    const [squareRotation, setSquareRotation] = useState(0);
+
+    const handleSquareRotationChange = (event) => {
+        setSquareRotation(parseInt(event.target.value));
+    };
+
+    var position = mouseToCoord(localMousePos.x, localMousePos.y)
+    var botPosition = coordToMouse(startPos[0], startPos[1])
+    var botSize = valToSize(squareWidth, squareHeight);
+
+    const [showText, setShowText] = useState(false);
+
+    // Handler function for toggling text display
+    const handleToggleText = () => {
+        setShowText((prevState) => !prevState);
+      };
+
     return (  
         <div>
         <div className='skills'>
         <ToggleSwitch label="Skills: " onToggle={handleToggle}/>
         </div>
         <div className='stats'>
-            Bot Width:
-            <form>
-                <input type='' />
-            </form>
-            Bot Length:
-            <form>
-                <input type='' />
-            </form>
-            <div className='save-button'>
-            <button className='button-29'>Save</button>
+            <div>
+            {/* Input fields for square width and height */}
+            <label htmlFor="squareWidth">Bot Width: </label>
+            <input
+                className="botStats"
+                type="number"
+                id="squareWidth"
+                value={squareWidth}
+                onChange={handleSquareWidthChange}
+            />
             </div>
+            <div>
+                <label htmlFor="squareHeight">Bot Length: </label>
+                <input
+                    className="botStats"
+                    type="number"
+                    id="squareHeight"
+                    value={squareHeight}
+                    onChange={handleSquareHeightChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="">X: </label>
+                <input
+                    className="botStats"
+                    type="number"
+                    id="startingX"
+                    value={startPos[0]}
+                    onChange={handleStartingPosX}
+                />
+                <label>Y: </label>
+                <input
+                    className="botStats"
+                    type="number"
+                    min="-72"
+                    max="72"
+                    id="startingY"
+                    value={startPos[1]}
+                    onChange={handleStartingPosY}
+                />
+                <label>θ: </label>
+                <input
+                    className="botStats"
+                    type="number"
+                    min="-360"
+                    max="360"
+                    id="angle"
+                    value={squareRotation}
+                    onChange={handleSquareRotationChange}
+                />
+            </div>
+            <div className="toggle-container">
+            Show text:
+                <input
+                    type="checkbox"
+                    id="toggleText"
+                    checked={showText}
+                    onChange={(e) => handleToggleText(e.target.checked)}
+                />
+                <label htmlFor="toggleText" className="toggle-label">
+                    <span className="toggle-text">
+                        {showText ? '' : ''}
+                    </span>
+                </label>
+            </div>
+            {/* <div className='save-button'>
+            <button className='button-29'>Save</button>
+            </div> */}
         </div>
         <div className='container_main'>
             {/* <form>
@@ -78,9 +246,17 @@ const Tracker = () => {
             <div className='coord'>
             Position:
             <b>
-                ({convert_coord(localMousePos.x, localMousePos.y)})
+                ({position})
             </b>
             </div>
+            <BotDrawer
+            x={(botPosition[0] - botSize[0] / 2)}
+            y={(botPosition[1] - botSize[1] / 2)}
+            width={botSize[0]}
+            height={botSize[1]}
+            rotation={squareRotation}
+            showText={showText}
+            />
         </div>
         </div>
     );
