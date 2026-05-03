cd2b77d4065766d2eb37348ac02c7300d9b65ae3
Edison Ying <edisony611@gmail.com>
2026-05-02 21:25:42 -0400

    update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> 
         <Route path="/tracker" element={<Tracker />} />
         <Route path="/path-record" element={<PathRecord />} />
         <Route path="/code-generator" element={<CodeGenerator />} />
